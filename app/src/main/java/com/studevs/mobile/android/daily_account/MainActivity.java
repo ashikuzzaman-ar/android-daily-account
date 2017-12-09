e9d4ca74013d93f5e71441d124a3f30c57df27e5
ashik <ashikuzzaman.ar@gmail.com>
2017-12-09 20:29:50 +0600

    Update : Added login & signup buttona & completing design

--- app/src/main/java/com/studevs/mobile/android/daily_account/MainActivity.java
+++ app/src/main/java/com/studevs/mobile/android/daily_account/MainActivity.java
@@ -1,13 +1,42 @@
 package com.studevs.mobile.android.daily_account;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+	
+	private EditText etUsername ;
+	private EditText etPassword ;
+	private Button btnSignup ;
+	private Button btnLogin ;
+	
+	private void initialization(){
+	
+		this.etUsername = this.findViewById(R.id.etUsername);
+		this.etPassword = this.findViewById(R.id.etPassword);
+		this.btnLogin = this.findViewById(R.id.btnLogin);
+		this.btnSignup = this.findViewById(R.id.btnSignup);
+	}
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		this.initialization();
+		
+		this.btnSignup.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				
+				Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+				startActivity(intent);
+			}
+		});
 	}
 }
