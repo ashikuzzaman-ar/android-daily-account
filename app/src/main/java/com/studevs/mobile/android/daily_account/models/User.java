29b348f527a1d690245708a061500ba14a6b20a9
ashik <ashikuzzaman.ar@gmail.com>
2017-12-09 16:22:21 +0600

    Update : Added Room Data Persistence Dependency

---
+++ app/src/main/java/com/studevs/mobile/android/daily_account/models/User.java
@@ -0,0 +1,12 @@
+package com.studevs.mobile.android.daily_account.models;
+
+import android.arch.persistence.room.Entity;
+
+import java.io.Serializable;
+
+/**
+ * Created by ashik on 12/9/17.
+ */
+@Entity
+public class User implements Serializable{
+}
